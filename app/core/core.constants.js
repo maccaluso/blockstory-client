3847b6d8bd80faea2f80b528a788f2b9e1b687ce
Gianluca Macaluso <maccaluso@gmail.com>
2015-11-23 10:04:36 +0100

    switch to production mode

--- app/core/core.constants.js
+++ app/core/core.constants.js
@@ -4,10 +4,10 @@
 
     angular
         .module('app.core')
-        .constant('storyID', '55c7e454c54d2d0e9601f821')
+        .constant('storyID', '56241f8e2ff172ce01732d04')
         .constant('baseHref', '/')
         .constant('ENV', {
-        	remote: false,
+        	remote: true,
         	mode: 'dev'
         })
 })();
